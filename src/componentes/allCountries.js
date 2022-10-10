b204f814740a2f058b093de8be23bc4fcafa3752
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-10-10 05:13:52 -0300

    adicionando css

--- src/componentes/allCountries.js
+++ src/componentes/allCountries.js
@@ -1,55 +1,50 @@
 import React, { useState, useEffect } from "react";
-import { apiURL } from "../util/api";
-
-import SearchInput from "../Search/SearchInput";
-import FilterCountry from "../FilterCountry/FilterCountry";
-
-import { Link } from "react-router-dom";
 
 const url = "https://restcountries.com/v2/all";
 
 const AllCountries = () => {
+  const [countries, setCountries] = useState([]);
 
-  const [countries, setCountries] = useState([])
-  const [Filtered, setFiltered] = useState([])
-  const [Search, setSearchInput] = useState("")
-    const [isLoading, setLoading] =useState(true)
-    
+  const [Filtered, setFiltered] = useState([]);
+  const [Search, setSearchInput] = useState("");
+  const [isLoading, setLoading] = useState(true);
+
+  const fetcCountries = async () => {
+    const response = await fetch(url);
+    const countries = await response.json();
+    setCountries(countries);
+  };
+
   useEffect(() => {
-    const fetcCountries = async () => {
-      const response = await fetch(url)
-      const countries = await response.json()
-      setCountries(countries)
-      setIsloading(false)
-    }
-    
-    fetchCountries()
-      },[])
+    fetcCountries();
+  }, []);
 
-    return (
-  <>
-      {AllCountries.map((country) => {
-        const { numericCode, name, population, region, capital, flag } =
-          country;
+  return (
+    <>
+      <section className="grid">
+        {countries.map((country) => {
+          const { name, population, region, capital, flag, numericCode } =
+            country;
 
-        return (
-          <>
-          <article article key={numericCode}>
-            <div>
-              <img src={flag} alt={name}></img>
-              <h3>{name}</h3>
-              <h4>
-                Population:<span>{population}</span>
-              </h4>
-              <h4>
-                Região:<span>{region}</span>
-              </h4>
-              <h4>
-                Capital:<span>{capital}</span>
-              </h4>
-            </div>
-          </article>
+          return (
+            <article key={numericCode}>
+              <div>
+                <img src={flag} alt="name" />
+                <div className="details">
+                  <h3>{name}</h3>
+                  <h4>
+                    População: <span> {population}</span>{" "}
+                  </h4>
+                  <h4>
+                    Região: <span>{region}</span>{" "}
+                  </h4>
+                  <h4>Capital: {capital}</h4>
+                </div>
+              </div>
+            </article>
+          );
         })}
+      </section>
     </>
   );
 };
