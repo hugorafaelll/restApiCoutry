7a28cb1359332e8d4ca426c31591f9da29d6b7ec
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-10-13 23:56:31 -0300

    adicionando react router dom ao sistema

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from "react";
 import AllCountries from "./componentes/allCountries";
 import Filter from "./componentes/filter";
 import Header from "./componentes/header";
+import { browserRouter as Router, Route } from "reac-router-dom";
 
 const url = "https://restcountries.com/";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <Filter />
-      <AllCountries />
-    </>
+
+      <Route>
+        <AllCountries />
+      </Route>
+    </Router>
   );
 }
 
