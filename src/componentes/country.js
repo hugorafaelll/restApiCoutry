b136fe458259af11aa13790e1d2171cca774e784
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-10-18 00:23:57 -0300

    tentando resolver o erro do fetch

--- src/componentes/country.js
+++ src/componentes/country.js
@@ -1,10 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 
 const Country = () => {
+  const [country, setCountry] = useState([]);
+  const { name } = useParams();
+
+  useEffect(() => {
+    const fetchCountryData = async () => {
+      const response = await fetch("https://randomfox.ca/?i=49");
+      const country = await response.json();
+      setCountry(country);
+    };
+    console.log(country);
+
+    fetchCountryData();
+  }, [name]);
+
   return (
     <>
-      <Link to="/">
+      <Link to="/" className="btn btn-light">
         <i className="fas fa-arrow-left"></i> Return
       </Link>
       <br />
--- src/componentes/header.js
+++ src/componentes/header.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { faMoon } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
   return (
@@ -10,7 +8,7 @@ const Header = () => {
           <h1>Where in the world?</h1>
         </div>
         <div>
-          <i className="fa-solid fa-moon"></i>
+          <i className="fa-sharp fa-solid fa-moon fa-li-margin"></i>
         </div>
       </header>
     </>
