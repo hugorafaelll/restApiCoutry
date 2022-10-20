7b696eba700e10e519441ee714b38acf66e40b7f
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-10-20 03:33:12 -0300

    utilizando a api

--- src/componentes/country.js
+++ src/componentes/country.js
@@ -7,22 +7,68 @@ const Country = () => {
 
   useEffect(() => {
     const fetchCountryData = async () => {
-      const response = await fetch("https://randomfox.ca/?i=49");
+      const response = await fetch(
+        `https://restcountries.com/v3.1/name/${name}`
+      );
       const country = await response.json();
       setCountry(country);
+      console.log(country);
     };
-    console.log(country);
 
     fetchCountryData();
   }, [name]);
 
   return (
     <>
-      <Link to="/" className="btn btn-light">
-        <i className="fas fa-arrow-left"></i> Return
-      </Link>
-      <br />
-      <h1>Nome do pais</h1>
+      <section className="country">
+        <Link to="/" className="btn btn-light">
+          <i className="fas fa-arrow-left"></i> Back Home
+        </Link>
+        {country.map((c) => {
+          const {
+            area,
+            flags,
+            capital,
+            population,
+            region,
+            subregion,
+            languages,
+          } = c;
+
+          return (
+            <article key={area}>
+              <div className="country-inner">
+                <div className="flag">
+                  <h2>
+                    Name:<span> {name}</span>
+                  </h2>
+                  <img src={flags.svg} />
+                </div>
+
+                <div className="country-details">
+                  <div>
+                    <h5>
+                      Capital :<span> {capital}</span>
+                    </h5>
+                    <h5>
+                      Region :<span>{region}</span>
+                    </h5>
+                    <h5>
+                      Sub-region :<span>{subregion}</span>
+                    </h5>
+                    <h5>
+                      Pupulation :<span> {population}</span>
+                    </h5>
+                    <h5>
+                      Languages:<span> {languages.prs}</span>
+                    </h5>
+                  </div>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </section>
     </>
   );
 };
